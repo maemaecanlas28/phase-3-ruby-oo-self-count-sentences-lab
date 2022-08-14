c55499eedf97d9a493cc42c9814f6a687b609db1
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-14 13:03:46 -0700

    commit tree 3dc34010e1e3f7c7117c8645eb02ac222602fdee

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    return self.split(/\!|\?|\./).count {|str| str != ""}
   end
 end
+
+puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
